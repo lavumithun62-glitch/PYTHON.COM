3ff05062171329814325c37e8135ab7451479bde
lavumithun62-glitch <lavumithun62@gmail.com>
2026-04-23 20:42:01 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.database import engine, Base
+from app.routers import router
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(router)
+
+@app.get("/")
+def home():
+    return {"message": "API working"}
